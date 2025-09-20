94a4453e820c05061f277c135021810818e78093
dj-io <daquanj11@gmail.com>
2025-09-20 18:15:39 -0400

    fix: (hero) -> mobile scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { HeroSection } from "@/components/hero";
 
 export default function Home() {
   return (
-    <main className="overflow-hidden md:overflow-auto">
+    <main className="h-[100dvh] overflow-hidden md:overflow-auto flex items-center justify-center">
       <HeroSection />
     </main>
   );
