118f78e99b39c06e96de1248eabc0d3ae87d790d
dj-io <daquanj11@gmail.com>
2025-09-20 18:37:21 -0400

    fix: (hero) -> overscroll none

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { HeroSection } from "@/components/hero";
 
 export default function Home() {
   return (
-    <main className="h-[100dvh] overflow-hidden md:overflow-auto flex items-center justify-center">
+    <main className="h-[100svh] overflow-hidden overscroll-none flex items-center justify-center">
       <HeroSection />
     </main>
   );
